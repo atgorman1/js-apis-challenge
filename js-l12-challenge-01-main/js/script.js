b41dc8abce2773490d5068c133a27156e32ed338
Alida Gorman <128799448+atgorman1@users.noreply.github.com>
2023-05-19 12:37:19 -0400

    Added API to get images from source

--- js-l12-challenge-01-main/js/script.js
+++ js-l12-challenge-01-main/js/script.js
@@ -2,3 +2,9 @@ const button = document.querySelector("button");
 const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
+const getImage = async function () {
+  res = await fetch("https://picsum.photos/v2/list?limit=100");
+  const images = await JSON();
+  console.log(images);
+};
+console.log(getImage);
